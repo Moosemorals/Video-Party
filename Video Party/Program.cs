ff45979fa44162ec02994c47c50e9e802342b65a
Osric Wilkinson <gitmoose@fluffypeople.com>
2021-03-06 13:50:34 +0000

    Added loging + metrics, setup some standard stuff.

--- Video Party/Program.cs
+++ Video Party/Program.cs
@@ -3,21 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Serilog;
+using Serilog.Events;
+
 namespace uk.osric.VideoParty {
     public class Program {
         public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .Enrich.WithProperty("App Name", "Video Party")
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try {
+                BuildWebHost(args).Run();
+                return;
+            } catch (Exception ex) {
+                Log.Fatal(ex, "WebHost terminated unexpectedly");
+                return;
+            } finally {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                   .UseStartup<Startup>()
+                   .UseSerilog()
+                   .UseKestrel(options => options.UseSystemd())
+                   .Build();
+
+
     }
 }
--- Video Party/Startup.cs
+++ Video Party/Startup.cs
@@ -6,29 +6,74 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Prometheus;
+
+using Serilog;
+
 namespace uk.osric.VideoParty {
     public class Startup {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services) {
+
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IWebHostEnvironment env) {
+            _env = env;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddHttpContextAccessor();
+
+            services.Configure<ForwardedHeadersOptions>(options => {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor
+                    | ForwardedHeaders.XForwardedProto;
+            });
+
+            services.AddMvcCore();
+
+            IMvcBuilder mvcBuilder = services.AddControllersWithViews(
+                options => {
+                    options.EnableEndpointRouting = false;
+
+                    options.Filters.Add(
+                        new AutoValidateAntiforgeryTokenAttribute()
+                    );
+                }
+            );
+
+            if (_env.IsDevelopment()) {
+                mvcBuilder.AddRazorRuntimeCompilation();
+            }
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+
+            app.UseSerilogRequestLogging();
+            app.UseForwardedHeaders();
+
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles(new StaticFileOptions {
+                OnPrepareResponse = ctx => ctx.Context.Response.Headers.Append("Cache-Control",
+                        $"public max-age={TimeSpan.FromHours(27).TotalSeconds}"),
+            });
+
+
             app.UseRouting();
+            app.UseHttpMetrics();
 
             app.UseEndpoints(endpoints => {
-                endpoints.MapGet("/", async context => {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
+                endpoints.MapMetrics();
             });
+
+            app.UseMvc();
+
         }
     }
 }
